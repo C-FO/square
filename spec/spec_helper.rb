eafa9eb12c1b5b729942ef7ff8f9ef566d395f55
Ryota Ikezawa <ikezawa-ryota@c-fo.com>
2018-07-27 13:43:35 +0900

    api_spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,61 @@
 require 'simplecov'
 SimpleCov.coverage_dir(File.join(ENV.fetch('CIRCLE_ARTIFACTS', 'public'), 'coverage'))
 SimpleCov.start do
-  %w[vendor/ spec/].each {|dir| add_filter(dir) }
+  %w[vendor/ spec/].each { |dir| add_filter(dir) }
 end
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'square'
+require 'webmock'
 require 'pry'
 
+include WebMock::API
+
+WebMock.disable_net_connect!(allow: 'coveralls.io')
+
+def endpoint_for(api_name)
+  case api_name
+  when :connect
+    Square::Connect::Default::ENDPOINT
+  else
+    ''
+  end
+end
+
+def a_get(api_name, path)
+  a_request(:get, endpoint_for(api_name) + path)
+end
+
+def a_post(api_name, path)
+  a_request(:post, endpoint_for(api_name) + path)
+end
+
+def a_put(api_name, path)
+  a_request(:put, endpoint_for(api_name) + path)
+end
+
+def a_delete(api_name, path)
+  a_request(:delete, endpoint_for(api_name) + path)
+end
+
+def stub_get(api_name, path)
+  stub_request(:get, endpoint_for(api_name) + path)
+end
+
+def stub_post(api_name, path)
+  stub_request(:post, endpoint_for(api_name) + path)
+end
+
+def stub_put(api_name, path)
+  stub_request(:put, endpoint_for(api_name) + path)
+end
+
+def stub_delete(api_name, path)
+  stub_request(:delete, endpoint_for(api_name) + path)
+end
+
 def fixture_path
-  File.expand_path('fixtures', __dir__)
+  File.expand_path('../fixtures', __FILE__)
 end
 
 def fixture(file)
